93efdc8c2ff1823947df46ff8346869ea1644068
Simon Kisimov <skisimov@gmail.com>
2017-03-07 16:33:19 +0300

    add article validations and spec (41m)

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
  
 RSpec.describe Article do
-  it { should have_many :comments }
+ 
+  describe "validations" do
+ 
+    it { should validate_presence_of :title }
+ 
+    it { should validate_presence_of :text }
+ 
+  end
+ 
+
+ 
+  describe "associations" do
+ 
+    it { should have_many :comments }
+ 
+  end
+ 
 end
