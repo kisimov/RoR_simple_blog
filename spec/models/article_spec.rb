ac3d78913a207688aed18992ca590990e0f13f55
Simon Kisimov <skisimov@gmail.com>
2017-03-09 11:11:31 +0300

    add commet spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,13 +1,9 @@
 require 'rails_helper'
  
 RSpec.describe Article do
- 
   describe "validations" do
- 
     it { should validate_presence_of :title }
- 
     it { should validate_presence_of :text }
- 
   end
 
   describe "#subject" do
@@ -15,5 +11,12 @@ RSpec.describe Article do
   		article = create(:article, title: 'Lorem ipsum')
   		expect(article.subject).to eq 'Lorem ipsum'
   	end
+  end
+
+  describe '#last_comment' do
+   it 'returns last comment' do
+     article = create(:article_with_comments) # наша новая фабрика
+     expect(article.last_comment).eq 'comment body 1'
+    end
   end
 end
